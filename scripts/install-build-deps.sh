aa740a79f6d2aa84278b95afddb6bf7a5b5a19b1
Uri Herrera <hello@uriherrera.com>
2026-09-24 08:17:41 -0600

    Add build dependency

--- scripts/install-build-deps.sh
+++ scripts/install-build-deps.sh
@@ -36,6 +36,7 @@ $APT_COMMAND install -y --no-install-recommends \
     gettext \
     gnupg2 \
     libkf6bluezqt-dev \
+    libkf6colorscheme-dev \
     libkf6coreaddons-dev \
     libkf6i18n-dev \
     libkf6notifications-dev \
